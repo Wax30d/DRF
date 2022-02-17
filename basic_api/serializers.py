3258b6164f5b738a77d0102e097ddfe8aed9991f
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-17 16:08:43 +0500

    Created a serializer. - Serializers are used to transform model instances or objects into JSON.

---
+++ basic_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from basic_api.models import DRFPost
+
+
+class DRFPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DRFPost
+        fields = '__all__'
